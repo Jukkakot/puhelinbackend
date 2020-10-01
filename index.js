ceee5313ab96928b132b998ff20c37cc811f38ab
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-10-01 13:02:40 +0300

    3.1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('hello world')
+
+  const express = require('express')
+  const app = express()
+    let persons = [
+        { 
+            "name": "Arto Hellas", 
+            "number": "040-123456",
+            "id": 1
+          },
+          { 
+            "name": "Ada Lovelace", 
+            "number": "39-44-5323523",
+            "id": 2
+          },
+          { 
+            "name": "Dan Abramov", 
+            "number": "12-43-234345",
+            "id": 3
+          },
+          { 
+            "name": "Mary Poppendieck", 
+            "number": "39-23-6423122",
+            "id": 4
+          },
+          { 
+            "name": "Jukka Kotilainen", 
+            "number": "123 123123123",
+            "id": 5
+          }
+          ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  app.get('/info', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
