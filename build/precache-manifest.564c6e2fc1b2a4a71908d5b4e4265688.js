dbf4a7771db040a30fe7569f8a0ae08bd3f2d6c6
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-10-01 17:15:39 +0300

    uibuild

---
+++ build/precache-manifest.564c6e2fc1b2a4a71908d5b4e4265688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c53e0ee8ebfc11d54f7b70064ff3b4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "783a5e5fb2e0465efab6",
+    "url": "/static/css/main.a4e597f4.chunk.css"
+  },
+  {
+    "revision": "36b255a929c5c09379bf",
+    "url": "/static/js/2.52064027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52064027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783a5e5fb2e0465efab6",
+    "url": "/static/js/main.60ba4b2f.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
